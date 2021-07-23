81a5d11414df7c7c6624eddf2423b8a744eee214
Priscilla Baah <pyfbaah@gmail.com>
2021-07-23 01:47:46 +0000

    get unique vowels in a string

---
+++ task-0.9.js
@@ -0,0 +1,22 @@
+// printing out vowels in a string //
+
+function vowels(string) {
+
+    string = string.toLowerCase();
+    const allVowels = ['a', 'e', 'i', 'o', 'u'];
+    var vowelsInString = new Set(); // using a set so that only unique vowels are stored//
+    var i = 0;
+
+    while (i < string.length) {
+        if (allVowels.includes(string[i])){
+            vowelsInString.add(string[i]);
+        };
+        i++;
+    };
+
+    vowelsInString = Array.from(vowelsInString).join(', '); // convert to a comma separated array representation"
+    console.log('Vowels: ', vowelsInString) 
+   
+}
+
+vowels('Enthusiastic')
