32403ded81888a4db96c38c20c4cac4eab4235c6
Priscilla Baah <pyfbaah@gmail.com>
2021-07-23 11:43:51 +0000

    find maximum number given any number of arguments

--- task-0.6.js
+++ task-0.6.js
@@ -1,18 +1,30 @@
-function maxNumber(num1, num2, num3){
-    if (num1 > num2){
-        if (num1 > num3){
-            return num1;
-        }
-        else {
-            return num3;
+function maxNumber(){
+
+    var i = 0;
+    const greaterThanList = [];
+
+    while (i < arguments.length){
+
+        var greaterThan = 0;
+        for (var j=0; j < arguments.length; j++){
+            if (arguments[i] >= arguments[j]){ 
+                greaterThan += 1;
             }
+        }
+
+        greaterThanList.push(greaterThan);
+        i++;
     }
-    else if (num2 > num3) {
-        return num2;
-    }
-    else {
-        return num3;
+    
+    i = 0;
+    while(i < arguments.length){
+    
+        if (greaterThanList[i] == arguments.length) {
+            return arguments[i];
+        };
+        i++;
+        
     }
 }
 
-console.log(maxNumber(1, 2, 3))
+console.log(maxNumber(7, 10, 39, 20, 15, 100, 100))
